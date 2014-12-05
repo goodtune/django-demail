ad1d60140d64546bc2e96f0f5f9825a729cdab5d
Gary Reynolds <gary@touch.asn.au>
2014-12-05 22:33:12 +1100

    Set development version number.

--- demail/__init__.py
+++ demail/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 NAME = "Delegate Email"
-__version__ = "0.3dev"
+__version__ = "0.4dev"
 
 logger = logging.getLogger(__name__)
 logger.debug('"%s"/"%s"' % (NAME, __version__))
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "0.3dev"
+version = "0.4dev"
 
 setup(
     name='django-demail',
